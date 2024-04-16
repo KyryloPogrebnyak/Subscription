721d8d7a57368841c0b2becf35f0816540af9761
KyryloPogrebnyak <120728576+KyryloPogrebnyak@users.noreply.github.com>
2024-04-16 18:54:39 +0200

    implemented views protecting

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login_view')
 def client_dashboard(request):
     return render(request, 'client/client-dashboard.html')
