a0207686168fecf776fb647ecc80d955c37f6687
KyryloPogrebnyak <120728576+KyryloPogrebnyak@users.noreply.github.com>
2024-05-08 16:20:10 +0200

    implement create-article for writer

--- subplatform/writer/models.py
+++ subplatform/writer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from account.models import CustomUser
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_premium = models.BooleanField(default=False, verbose_name='Premium article')
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(CustomUser, max_length=10, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.title
--- subplatform/writer/views.py
+++ subplatform/writer/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .forms import ArticleForm
+from django.http import HttpResponse
 # Create your views here.
 
 @login_required(login_url='login_view')
 def writer_dashboard(request):
     return render(request, 'writer/writer-dashboard.html')
+
+@login_required(login_url='login_view')
+def create_article(request):
+
+    form = ArticleForm()
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return HttpResponse('Article created successfully')
+    
+    context = {'CreateArticleForm': form}
+    return render(request, 'writer/create-article.html', context)
