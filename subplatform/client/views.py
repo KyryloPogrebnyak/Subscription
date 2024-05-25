6b6c37bb2dff18e380a185764d34da11491b2b57
KyryloPogrebnyak <120728576+KyryloPogrebnyak@users.noreply.github.com>
2024-05-25 14:14:54 +0200

    subscription pre setup - done

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from writer.models import Article
+from .models import Subscription
 
 
 @login_required(login_url="login_view")
 def client_dashboard(request):
-    return render(request, "client/client-dashboard.html")
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+        subscription_plan = subDetails.subscription_plan
+    except Subscription.DoesNotExist:
+        subscription_plan = "No Subscription"
+
+    context = {"SubPlan": subscription_plan}
+    return render(request, "client/client-dashboard.html", context)
+
+
+@login_required(login_url="login_view")
+def browse_articles(request):
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+    except Subscription.DoesNotExist:
+        return render(request, "client/subscription-locked.html")
+
+    current_subscription_plan = subDetails.subscription_plan
+
+    if current_subscription_plan == "Standard":
+        articles = Article.objects.all().filter(is_premium=False)
+    elif current_subscription_plan == "Premium":
+        articles = Article.objects.all()
+
+    context = {"AllClientArticles": articles}
+    return render(request, "client/browse-articles.html", context)
+
+
+@login_required(login_url="login_view")
+def subscription_locked(request):
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+        subscription_plan = subDetails.subscription_plan
+    except Subscription.DoesNotExist:
+        subscription_plan = "No Subscription"
+
+    context = {"SubPlan": subscription_plan}
+    return render(request, "client/subscription-locked.html", context)
